725a3a3ae922c0928863b7903ecd2a03c82486ae
asilverrrr <andreu.slynchyk@gmail.com>
2025-01-14 20:57:56 +0200

    add animation

---
+++ src/app/MainWrapper.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const MainWrapper = (props: { children: React.ReactNode }) => {
+  return (
+    <main className="relative flex h-full w-full bg-[#181922] text-white">
+      <div
+        className={
+          "animate-ripple absolute left-1/2 top-1/2 h-[50vw] w-[50vw] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#5e5d5d1a]"
+        }
+      />
+      <div
+        className={
+          "animate-ripple absolute left-1/2 top-1/2 h-[50vw] w-[50vw] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#5e5d5d1a]"
+        }
+        style={{ animationDelay: "2s" }}
+      />
+      <div
+        className={
+          "animate-ripple absolute left-1/2 top-1/2 h-[50vw] w-[50vw] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#5e5d5d1a]"
+        }
+        style={{ animationDelay: "4s" }}
+      />
+      <div
+        className={
+          "flex h-full min-h-screen w-full flex-col items-center pt-40"
+        }
+      >
+        {props.children}
+      </div>
+    </main>
+  );
+};
+
+export default MainWrapper;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,34 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      keyframes: {
+        ripple: {
+          "0%": {
+            transform: "translate(-50%, -50%) scale(1)",
+            opacity: "1",
+          },
+          "50%": {
+            transform: "translate(-50%, -50%) scale(4)",
+            opacity: "0",
+          },
+          "80%": {
+            transform: "translate(-50%, -50%) scale(1)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "translate(-50%, -50%) scale(1)",
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        ripple: "ripple 20s infinite ease-in-out",
+      },
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+      colors: {
+        main: "#5e5d5d1a",
       },
     },
   },
