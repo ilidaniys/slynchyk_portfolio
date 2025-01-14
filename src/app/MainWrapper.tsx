b110ecc8f5edc9f480577cdadc9f1fc2717ef82a
Andreu <andreu.slycnhyk@gmail.com>
2025-01-14 22:54:36 +0200

    add wrapper

--- src/app/MainWrapper.tsx
+++ src/app/MainWrapper.tsx
@@ -3,6 +3,7 @@ import React from "react";
 const MainWrapper = (props: { children: React.ReactNode }) => {
   return (
     <main className="relative flex h-full w-full bg-[#181922] text-white">
+        <div className={"absolute overflow-hidden w-full h-full"}>
       <div
         className={
           "animate-ripple absolute left-1/2 top-1/2 h-[50vw] w-[50vw] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#5e5d5d1a]"
@@ -20,6 +21,7 @@ const MainWrapper = (props: { children: React.ReactNode }) => {
         }
         style={{ animationDelay: "4s" }}
       />
+        </div>
       <div
         className={
           "flex h-full min-h-screen w-full flex-col items-center pt-40"
