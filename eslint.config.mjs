a89495b8da4b6d42de7072d7ce8c6500fb6c48ad
asilverrrr <andreu.slynchyk@gmail.com>
2026-02-18 21:36:23 +0700

    update next/lint

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+/** @type {import("eslint").Linter.Config[]} */
+const config = [
+  ...compat.config({
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: true,
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+      "next/core-web-vitals",
+      "plugin:@typescript-eslint/recommended-type-checked",
+      "plugin:@typescript-eslint/stylistic-type-checked",
+    ],
+    rules: {
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+    },
+  }),
+];
+
+export default config;
